9b19caae68fc923755d2afdb439461d56ffb3f03
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-01-14 16:14:44 +0100

    Add clear selected functionality

--- modules/Completed.js
+++ modules/Completed.js
@@ -1,9 +1,23 @@
-const markCompleted = () => {
-  // for completed
+import { renderTodos } from './renderTodos.js';
+import Storage from './storage.js';
+import Todos from './todosClass.js';
+
+const reRender = (listContainer) => {
+  const myDisplay = renderTodos();
+  listContainer.innerHTML = myDisplay;
 };
 
-const clearCompleted = () => {
-  // remove completed
+const markCompleted = (checkbox, id, listContainer) => {
+  const { checked } = checkbox;
+  const toDoList = Storage.getData();
+  toDoList[id - 1].completed = checked;
+  Storage.storeData(toDoList);
+  reRender(listContainer);
+};
+
+const clearCompleted = (listContainer) => {
+  Todos.removeTodo(0, true);
+  reRender(listContainer);
 };
 
 export { markCompleted, clearCompleted };
